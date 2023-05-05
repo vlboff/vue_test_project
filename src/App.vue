051a3fe100dd91a03b54ca4290cdceb8bbcefc9b
Uladzimir <vl.barbikov@gmail.com>
2023-05-05 20:24:09 +0300

    feat: add posts request

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,49 @@
 <template>
-  <div>
-    <button @click="addLikes">Add like</button>
-    <button @click="addDislikes">Add dislike</button>
-    <div>Likes: {{likes}}</div>
-    <div>Dislikes: {{dislikes}}</div>
-  </div>
+  <main>
+    <div class="post" v-for="post in posts" v-bind:key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>
+        {{ post.body }}
+      </p>
+    </div>
+  </main>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
+const makeRequest = async () => fetch("https://dummyjson.com/posts")
+  .then((res) => res.json()).then((res) => res.posts);
+
 export default defineComponent({
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [],
     };
   },
-  methods: {
-    addLikes() {
-      this.likes += 1;
-    },
-    addDislikes() {
-      this.dislikes += 1;
-    },
+  async mounted() {
+    const getPosts = await makeRequest();
+    this.posts = getPosts;
   },
 });
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+main {
+  padding: 20px;
+  display: grid;
+  gap: 50px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid green;
+}
 </style>
