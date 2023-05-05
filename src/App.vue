0650e9997586ac1ea04ef89cdae3afecc2492c06
Uladzimir <vl.barbikov@gmail.com>
2023-05-05 18:06:50 +0300

    init: start project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <button @click="addLikes">Add like</button>
+    <button @click="addDislikes">Add dislike</button>
+    <div>Likes: {{likes}}</div>
+    <div>Dislikes: {{dislikes}}</div>
+  </div>
 </template>
 
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLikes() {
+      this.likes += 1;
+    },
+    addDislikes() {
+      this.dislikes += 1;
+    },
+  },
+});
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
